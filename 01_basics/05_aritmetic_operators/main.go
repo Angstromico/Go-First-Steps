7ffbe7b3a512e713818b0f1c66e695d3d11b6217
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-05-27 11:18:25 -0400

    feat: basic aritmetics

---
+++ 01_basics/05_aritmetic_operators/main.go
@@ -0,0 +1,18 @@
+package main
+
+func main() {
+	// Arithmetic operators
+	a := 10
+	b := 5
+	c := a + b // Addition
+	d := a - b // Subtraction
+	e := a * b // Multiplication
+	f := a / b // Division
+	g := a % b // Modulus
+
+	println("Addition:", c)
+	println("Subtraction:", d)
+	println("Multiplication:", e)
+	println("Division:", f)
+	println("Modulus:", g)
+}
