7fc7e9b4c5b25461bc2fc88a4a1036417abb8c6b
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-05-27 16:35:41 -0400

    feat: generate random number in guessing game

--- 03_features/01_guessing_game/main.go
+++ 03_features/01_guessing_game/main.go
@@ -1,22 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
-	// Guessing game
-	const secretNumber = 7
+	// Seed the random generator
+	rand.Seed(time.Now().UnixNano())
+
+	// Generate random number between 1 and 10
+	secretNumber := rand.Intn(10) + 1
+
 	var guess int
-	println("Welcome to the Guessing Game!")
-	println("Try to guess the secret number between 1 and 10.")
-	println("Enter your guess:")
+	fmt.Println("Welcome to the Guessing Game!")
+	fmt.Println("Try to guess the secret number between 1 and 10.")
+	fmt.Print("Enter your guess: ")
+
 	_, err := fmt.Scan(&guess)
 	if err != nil {
-		println("Error reading input")
+		fmt.Println("Error reading input")
 		return
 	}
+
 	if guess == secretNumber {
-		println("Congratulations! You guessed the secret number.")
+		fmt.Println("Congratulations! You guessed the secret number.")
 	} else {
-		println("Sorry, that's not the secret number.")
+		fmt.Println("Sorry, that's not the secret number.")
+		fmt.Println("The secret number was:", secretNumber) // optional for debugging
 	}
 }
